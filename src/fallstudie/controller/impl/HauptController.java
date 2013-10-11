94d77df1849f846e7dbb1ee1db21df76f49c737c
DanJoeHa <johannes.d.haag@web.de>
2013-10-11 17:30:11 +0200

    Kommentare und Mitarbeiter.ausloggen()

--- src/fallstudie/controller/impl/HauptController.java
+++ src/fallstudie/controller/impl/HauptController.java
@@ -7,9 +7,17 @@ import javax.swing.event.TreeSelectionListener;
 import javax.swing.tree.TreePath;
 
 import fallstudie.controller.interfaces.Controller;
-import fallstudie.model.interfaces.Mitarbeiter;
+import fallstudie.model.impl.Mitarbeiter;
 import fallstudie.view.interfaces.View;
 
+
+/**
+ * Hauptsteuerung des Programm, steuert das Programm anhand des Menuebaums und hoert auf Buttons "Logout" und "Hilfe"
+ *  
+ * @author Marc, Johannes, Angelos, Katrin, Ralf
+ * @version 0.1
+ *
+ */
 public class HauptController implements Controller, TreeSelectionListener {
 
 	private Controller activeController;
@@ -17,16 +25,34 @@ public class HauptController implements Controller, TreeSelectionListener {
 	protected static Hauptfenster hauptfenster;
 	protected static Mitarbeiter activeUser;
 	
+	/**
+	 * Hoert auf Buttons "Logout" und "Hilfe"
+	 * 
+	 * @author Marc
+	 * @version 0.1 - Aktionen für Buttons "Logout" und "Hilfe" gesetzt
+	 * 
+	 * @author Johannes
+	 * @version 0.2 - Nach Klärung mt Philip: Mitarbeiter erst ausloggen (Timestamp in DB gesetzt), dann View auf login
+	 */
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
+		//Aktion auslesen
 		String button = e.getActionCommand();
+		
+		//Wenn Button "Logout" gedrückt wurde
 		if(button == "Logout")
 		{
-			activeController = new LoginController();
-			//ausloggen() Mitarbeiter in DB?
-			hauptfenster.setContent(activeController.getView());
+			//Mitarbeiter ausloggen
+			if( activeUser.ausloggen() ){
+				
+				//LoginController erstellen und Sicht auf Login wechseln
+				activeController = new LoginController();
+				hauptfenster.setContent(activeController.getView());
+			}
+				
 		}
+		
+		//Wenn Button "Hilfe gedrückt wurde"
 		if(button == "Hilfe")
 		{
 			//internalFrame oder Popup? 
@@ -41,7 +67,7 @@ public class HauptController implements Controller, TreeSelectionListener {
 		String aktion = path.getLastPathComponent().toString();
 		switch(aktion)
 		{
-			case "Passwort �ndern":
+			case "Passwort �ndern":
 					activeController = new PasswortAendernController();
 					hauptfenster.setContent(activeController.getView());
 					break;
@@ -64,7 +90,7 @@ public class HauptController implements Controller, TreeSelectionListener {
 			case "Th":
 					break;
 			default:
-				System.out.println("Keine definierte Aktion ausgew�hlt!");
+				System.out.println("Keine definierte Aktion ausgew�hlt!");
 		}
 	}
 
