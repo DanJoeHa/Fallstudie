87bf636bc4a8c2f2c6c0deefdc7fe023cd2d6c64
Sari S <Sari S@141.31.68.39>
2013-10-11 11:26:51 +0200

    Erste Version

---
+++ src/fallstudie/view/impl/Popup_speichern.java
@@ -0,0 +1,21 @@
+package fallstudie.view.impl;
+import javax.swing.JOptionPane;
+ 
+public class Popup_speichern{
+        public static void main(String[] bla){
+ 
+                // Erstellung Array vom Datentyp Object, Hinzufügen der Optionen               
+                Object[] options = {"Abbrechen", "Löschen"};
+ 
+                int selected = JOptionPane.showOptionDialog(null,
+                                                            "Wollen Sie wirklich speichern?",
+                                                            "Alternativen",
+                                                            JOptionPane.DEFAULT_OPTION, 
+                                                            JOptionPane.INFORMATION_MESSAGE, 
+                                                            null, options, options[0]);
+                System.out.println(selected);
+ 
+                System.exit(0);
+ 
+        }
+}
