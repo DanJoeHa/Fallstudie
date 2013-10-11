7a7788e713a534ff802f972d32f4a0d291225af3
MarcMoessinger <marc_24_12@web.de>
2013-10-11 16:13:53 +0200

    Controller Beginn

---
+++ src/fallstudie/controller/impl/PasswortAendernController.java
@@ -0,0 +1,36 @@
+package fallstudie.controller.impl;
+import java.awt.event.ActionEvent;
+
+public class PasswortAendernController extends HauptController {
+
+		public PasswortAendernController(){
+			this.activeView = new PasswortAendernView();
+		}
+		
+		public void actionPerformed(ActionEvent e) {
+			String button = e.getActionCommand();
+			if(button == "Speichern")
+			{
+				String neuesPasswort = this.activeView.getNeuesPasswort();
+				String wdhPasswort = this.activeView.getWdhPasswort();
+				String altesPasswort =this.activeView.getAltesPasswort();
+				
+				if(altesPasswort.equals((activeUser.getPasswort()){
+					//ändern aufgrund Verschlüsselung
+					if(wdhPasswort.equals(neuesPasswort)){
+						activeUser.setPasswort(neuesPasswort);
+						hauptfenster.setInfoBox("Passwort erfolgreich geändert");
+					}
+					else
+					{
+						hauptfenster.setInfoBox("Passwörter stimmen nicht überein");
+					}
+				}
+				else
+				{
+					hauptfenster.setInfoBox("Altes Passwort falsch");
+				}
+			}
+			
+		}
+}
