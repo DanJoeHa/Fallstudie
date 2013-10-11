951e98d8ca3394e010454b8557b205d3b006a820
Johannes <johannes.d.haag@web.de>
2013-10-11 22:45:03 +0200

    Treiber wieder hergestellt, Bitte nicht wieder löschen...

---
+++ src/fallstudie/Fallstudie.java
@@ -0,0 +1,25 @@
+package fallstudie;
+
+import fallstudie.controller.impl.HauptController;
+
+
+/**
+ * Initiiert das Programm
+ * 
+ * @author Johannes Haag
+ * @version 1.0
+ *
+ */
+public class Fallstudie {
+
+	/**
+	 * Ruft den Hauptcontroller auf
+	 * 
+	 * @author Johannes Haag
+	 * @version 1.0
+	 */
+	public static void main(String[] args) {
+		new HauptController();
+	}
+
+}
