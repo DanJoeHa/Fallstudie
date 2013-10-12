ea8a41b8df536d294a2ebf95976d05778f9bef6d
Philipp <philipp.kirchniawy@gmail.com>
2013-10-12 13:47:19 +0200

    erstellt

--- src/fallstudie/model/impl/TesterKlasse.java
+++ src/fallstudie/model/impl/TesterKlasse.java
@@ -6,8 +6,8 @@ public class TesterKlasse {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		Arbeitsgruppe ag = new Arbeitsgruppe(2);
-		System.out.println(ag.getAktiv()+"+"+ag.getID()+""+ag.getKurzbezeichnung());
-		}
+		
+	
+	}
 
 }
