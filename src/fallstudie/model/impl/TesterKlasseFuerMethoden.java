9884bd3a951af2d18ab26cb0d5deb9487ecb2335
Philipp <philipp.kirchniawy@gmail.com>
2013-10-12 08:37:35 +0200

    test

--- src/fallstudie/model/impl/TesterKlasseFuerMethoden.java
+++ src/fallstudie/model/impl/TesterKlasseFuerMethoden.java
@@ -8,9 +8,10 @@ public class TesterKlasseFuerMethoden {
 	 * @param args
 	 * @throws SQLException 
 	 */
-	public static void main(String[] args) throws SQLException {
+	public static void main(String[] args) throws SQLException, ClassNotFoundException {
 		int id=2;
-		Arbeitsgruppe ag = new Arbeitsgruppe(id);
+		Arbeitsgruppe ag;
+		ag = new Arbeitsgruppe(id);
 		String beschreibung = ag.getBeschreibung();
 		System.err.println(beschreibung);
 }
