65c2f820a39293421aa848acebcee77db24d1f95
nataljat <natalja.tr@web.de>
2013-10-13 18:46:50 +0200

    Größe Angepasst

--- src/fallstudie/view/impl/ArbeitsgruppeBearbeitenAnlegenView.java
+++ src/fallstudie/view/impl/ArbeitsgruppeBearbeitenAnlegenView.java
@@ -13,58 +13,52 @@ import javax.swing.JTextField;
 import javax.swing.JComboBox;
 import javax.swing.JButton;
 
-public class arbeitsgruppe_anleger extends JPanel {
+public class ArbeitsgruppeBearbeitenAnlegenView extends JPanel {
 	private JTextField T_AG_Leiter_Suchbegriff;
 
 	/**
 	 * Create the panel.
 	 */
-	public arbeitsgruppe_anleger() {
-		setPreferredSize(new Dimension(600, 700));
-		setMinimumSize(new Dimension(600, 700));
-		setMaximumSize(new Dimension(600, 700));
+	public ArbeitsgruppeBearbeitenAnlegenView() {
+		setPreferredSize(new Dimension(620, 660));
+		setMinimumSize(new Dimension(620, 660));
+		setMaximumSize(new Dimension(620, 660));
 		setFont(new Font("Tahoma", Font.PLAIN, 14));
-		setBounds(new Rectangle(0, 0, 600, 700));
+		setBounds(new Rectangle(0, 0, 620, 660));
 		setBorder(new LineBorder(new Color(0, 0, 0)));
 		setBackground(SystemColor.window);
 		setLayout(null);
 		
-		//L_Ueberschrift
-		JLabel L_Ueberschrift = new JLabel("Arbeitsgruppe anlegen");
-		L_Ueberschrift.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		L_Ueberschrift.setBounds(10, 11, 580, 17);
-		add(L_Ueberschrift);
-		
 		//L_AG_Leiter
 		JLabel L_AG_Leiter = new JLabel("Gruppenleiter:");
 		L_AG_Leiter.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		L_AG_Leiter.setBounds(10, 192, 210, 17);
+		L_AG_Leiter.setBounds(30, 200, 150, 30);
 		add(L_AG_Leiter);
 		
 		//L_Bereich
 		JLabel L_Bereich = new JLabel("Bereich:");
 		L_Bereich.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		L_Bereich.setBounds(10, 241, 210, 17);
+		L_Bereich.setBounds(30, 250, 150, 30);
 		add(L_Bereich);
 		
 		//T_AG_Leiter_Suchbegriff
 		T_AG_Leiter_Suchbegriff = new JTextField();
 		T_AG_Leiter_Suchbegriff.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		T_AG_Leiter_Suchbegriff.setColumns(10);
-		T_AG_Leiter_Suchbegriff.setBounds(175, 192, 181, 20);
+		T_AG_Leiter_Suchbegriff.setBounds(200, 200, 220, 30);
 		add(T_AG_Leiter_Suchbegriff);
 		
 		//C_Bereich
 		JComboBox C_Bereich = new JComboBox();
 		C_Bereich.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		C_Bereich.setBounds(175, 241, 181, 20);
+		C_Bereich.setBounds(200, 250, 300, 30);
 		add(C_Bereich);
 		
 		//B_AG_Leiter_Suche mit Lupe
-		JButton B_AG_Leiter_Suche = new JButton("suchen");
+		JButton B_AG_Leiter_Suche = new JButton("Suchen");
 		B_AG_Leiter_Suche.setIcon(null);
 		B_AG_Leiter_Suche.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		B_AG_Leiter_Suche.setBounds(366, 191, 106, 23);
+		B_AG_Leiter_Suche.setBounds(440, 200, 150, 30);
 		add(B_AG_Leiter_Suche);
 		
 
