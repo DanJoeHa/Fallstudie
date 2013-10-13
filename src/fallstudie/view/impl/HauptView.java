60f2e62400e47f9a600a7e1b8645565c5ece8515
DanJoeHa <johannes.d.haag@web.de>
2013-10-13 18:46:28 +0200

    NavTree wird anhand Rollen erstellt

--- src/fallstudie/view/impl/HauptView.java
+++ src/fallstudie/view/impl/HauptView.java
@@ -21,7 +21,7 @@ import fallstudie.view.interfaces.View;
 public class HauptView extends JFrame implements View{
 
 	private static final long serialVersionUID = 999907361524491947L;
-	private JPanel Content;
+	private JPanel Content, NavigationBackground;
 	private JButton B_Logout, B_Hilfe;
 	private JTextPane InfoBox;
 	private JTree Navigation;
@@ -47,64 +47,12 @@ public class HauptView extends JFrame implements View{
 		Panel.setLayout(null);
 		
 		//Hintergrundpanel Navigation
-		JPanel NavigationBackground = new JPanel();
+		NavigationBackground = new JPanel();
 		NavigationBackground.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		NavigationBackground.setBackground(SystemColor.inactiveCaption);
 		NavigationBackground.setBorder(new LineBorder(new Color(0, 0, 0)));
 		NavigationBackground.setBounds(9, 12, 190, 700);
 		Panel.add(NavigationBackground);
-		
-		//Navigationsbaum
-		DefaultMutableTreeNode root = new DefaultMutableTreeNode("Aktionen");
-		
-		DefaultMutableTreeNode erfassen = new DefaultMutableTreeNode("Daten erfassen");
-		root.add(erfassen);
-		DefaultMutableTreeNode anzeigen = new DefaultMutableTreeNode("Daten anzeigen");
-		root.add(anzeigen);
-		
-		DefaultMutableTreeNode arbeitsgruppe = new DefaultMutableTreeNode("Arbeitsgruppe");
-		root.add(arbeitsgruppe);
-		DefaultMutableTreeNode anlegenAG = new DefaultMutableTreeNode("anlegen");
-		arbeitsgruppe.add(anlegenAG);
-		DefaultMutableTreeNode bearbeitenAG = new DefaultMutableTreeNode("bearbeiten");
-		arbeitsgruppe.add(bearbeitenAG);
-		DefaultMutableTreeNode loescheAG = new DefaultMutableTreeNode("löschen");
-		arbeitsgruppe.add(loescheAG);
-		
-		DefaultMutableTreeNode art = new DefaultMutableTreeNode("Art");
-		root.add(art);
-		DefaultMutableTreeNode anlegenArt = new DefaultMutableTreeNode("anlegen");
-		art.add(anlegenArt);
-		DefaultMutableTreeNode loescheArt = new DefaultMutableTreeNode("löschen");
-		art.add(loescheArt);
-		
-		DefaultMutableTreeNode bereich = new DefaultMutableTreeNode("Bereich");
-		root.add(bereich);
-		DefaultMutableTreeNode anlegenBereich = new DefaultMutableTreeNode("anlegen");
-		bereich.add(anlegenBereich);
-		DefaultMutableTreeNode bearbeitenBereich = new DefaultMutableTreeNode("bearbeiten");
-		bereich.add(bearbeitenBereich);
-		DefaultMutableTreeNode loescheBereich = new DefaultMutableTreeNode("löschen");
-		bereich.add(loescheBereich);
-		
-		DefaultMutableTreeNode mitarbeiter = new DefaultMutableTreeNode("Mitarbeiter");
-		root.add(mitarbeiter);
-		DefaultMutableTreeNode anlegenMA = new DefaultMutableTreeNode("anlegen");
-		mitarbeiter.add(anlegenMA);
-		DefaultMutableTreeNode bearbeiteMA = new DefaultMutableTreeNode("bearbeiten");
-		mitarbeiter.add(bearbeiteMA);
-		DefaultMutableTreeNode loescheMA = new DefaultMutableTreeNode("löschen");
-		mitarbeiter.add(loescheMA);
-		
-		DefaultMutableTreeNode changepw = new DefaultMutableTreeNode("Passwort ändern");
-		root.add(changepw);
-		
-		DefaultMutableTreeNode jobconfig = new DefaultMutableTreeNode("Job-Einstellungen");
-		root.add(jobconfig);
-		
-		this.Navigation = new JTree(root);
-		this.Navigation.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		NavigationBackground.add(this.Navigation);
 		
 		//Logout-Button
 		B_Logout = new JButton("Logout");
@@ -144,27 +92,115 @@ public class HauptView extends JFrame implements View{
 	
 	public void setSachbearbeiter(){
 		this.B_Logout.setVisible(true);
+		this.createNavTree(true, false, false, false, false, false, false);
 	}
 	
 	public void setGruppenleiter(){
 		this.B_Logout.setVisible(true);
+		this.createNavTree(true, true, false, false, false, false, false);
 	}
 	
 	public void setBereichsleiter(){
 		this.B_Logout.setVisible(true);
+		this.createNavTree(false, true, false, false, false, false, false);
 	}
 	
 	public void setZentralbereichsleiter(){
 		this.B_Logout.setVisible(true);
+		this.createNavTree(false, true, false, false, false, false, false);
 	}
 	
 	public void setStabstelle(){
 		this.B_Logout.setVisible(true);
+		this.createNavTree(false, true, true, true, true, true, true);
+	}
+	
+	/**
+	 * Erstellt Navigationsbaum anhand der Rechte des Users - evtl. anhand Rechte in Controller machen?
+	 * 
+	 * @author Johannes
+	 * @version 0.1
+	 * 
+	 * @param r2datenerfassen
+	 * @param r2datenanzeigen
+	 * @param r2arbeitsgruppe
+	 * @param r2art
+	 * @param r2bereich
+	 * @param r2mitarbeiter
+	 * @param r2jobconfig
+	 */
+	public void createNavTree(boolean r2datenerfassen, boolean r2datenanzeigen, boolean r2arbeitsgruppe, boolean r2art, boolean r2bereich, boolean r2mitarbeiter, boolean r2jobconfig){
+		
+		DefaultMutableTreeNode root = new DefaultMutableTreeNode("Aktionen");
+		
+		if( r2datenerfassen ){
+			DefaultMutableTreeNode erfassen = new DefaultMutableTreeNode("Daten erfassen");
+			root.add(erfassen);
+		}
+		
+		if( r2datenanzeigen ){
+			DefaultMutableTreeNode anzeigen = new DefaultMutableTreeNode("Daten anzeigen");
+			root.add(anzeigen);
+		}
+		
+		if( r2arbeitsgruppe ){
+			DefaultMutableTreeNode arbeitsgruppe = new DefaultMutableTreeNode("Arbeitsgruppe");
+			root.add(arbeitsgruppe);
+			DefaultMutableTreeNode anlegenAG = new DefaultMutableTreeNode("anlegen");
+			arbeitsgruppe.add(anlegenAG);
+			DefaultMutableTreeNode bearbeitenAG = new DefaultMutableTreeNode("bearbeiten");
+			arbeitsgruppe.add(bearbeitenAG);
+			DefaultMutableTreeNode loescheAG = new DefaultMutableTreeNode("löschen");
+			arbeitsgruppe.add(loescheAG);
+		}
+		
+		if( r2art ){
+			DefaultMutableTreeNode art = new DefaultMutableTreeNode("Art");
+			root.add(art);
+			DefaultMutableTreeNode anlegenArt = new DefaultMutableTreeNode("anlegen");
+			art.add(anlegenArt);
+			DefaultMutableTreeNode loescheArt = new DefaultMutableTreeNode("löschen");
+			art.add(loescheArt);
+		}
+		
+		if( r2bereich ){
+			DefaultMutableTreeNode bereich = new DefaultMutableTreeNode("Bereich");
+			root.add(bereich);
+			DefaultMutableTreeNode anlegenBereich = new DefaultMutableTreeNode("anlegen");
+			bereich.add(anlegenBereich);
+			DefaultMutableTreeNode bearbeitenBereich = new DefaultMutableTreeNode("bearbeiten");
+			bereich.add(bearbeitenBereich);
+			DefaultMutableTreeNode loescheBereich = new DefaultMutableTreeNode("löschen");
+			bereich.add(loescheBereich);
+		}
+		
+		if( r2mitarbeiter ){
+			DefaultMutableTreeNode mitarbeiter = new DefaultMutableTreeNode("Mitarbeiter");
+			root.add(mitarbeiter);
+			DefaultMutableTreeNode anlegenMA = new DefaultMutableTreeNode("anlegen");
+			mitarbeiter.add(anlegenMA);
+			DefaultMutableTreeNode bearbeiteMA = new DefaultMutableTreeNode("bearbeiten");
+			mitarbeiter.add(bearbeiteMA);
+			DefaultMutableTreeNode loescheMA = new DefaultMutableTreeNode("löschen");
+			mitarbeiter.add(loescheMA);
+		}
+		
+		DefaultMutableTreeNode changepw = new DefaultMutableTreeNode("Passwort ändern");
+		root.add(changepw);
+		
+		if( r2jobconfig ){
+			DefaultMutableTreeNode jobconfig = new DefaultMutableTreeNode("Job-Einstellungen");
+			root.add(jobconfig);
+		}
+		
+		this.Navigation = new JTree(root);
+		this.Navigation.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		NavigationBackground.add(this.Navigation);
 	}
 	
 	public void setLoggedOut(){
 		this.B_Logout.setVisible( false );
-		this.Navigation.setVisible(false);
+		this.Navigation = null;
 	}
 	
 	public void setUeberschrift(String ueberschrift){
@@ -192,9 +228,15 @@ public class HauptView extends JFrame implements View{
 	
 	@Override
 	public void setController(Controller c) {
-		this.B_Logout.addActionListener(c);
-		this.B_Hilfe.addActionListener(c);
-		this.Navigation.addTreeSelectionListener((TreeSelectionListener) c);
+		
+		try{
+			this.B_Logout.addActionListener(c);
+			this.B_Hilfe.addActionListener(c);
+			this.Navigation.addTreeSelectionListener((TreeSelectionListener) c);
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		
 	}
 
 	@Override
