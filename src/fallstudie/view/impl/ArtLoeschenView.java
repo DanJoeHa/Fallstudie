ed2efbd5cc7e6c49b34a66cd1791b23267798ab7
MarcMoessinger <marc_24_12@web.de>
2013-10-14 14:24:54 +0200

    ArtLoeschenView

--- src/fallstudie/view/impl/ArtLoeschenView.java
+++ src/fallstudie/view/impl/ArtLoeschenView.java
@@ -31,9 +31,9 @@ public class ArtLoeschenView extends JPanel implements View{
 	 * Create the panel.
 	 */
 	public ArtLoeschenView() {
-		setPreferredSize(new Dimension(620, 660));
-		setMinimumSize(new Dimension(620, 660));
-		setMaximumSize(new Dimension(620, 660));
+		setPreferredSize(new Dimension(620, 655));
+		setMinimumSize(new Dimension(620, 655));
+		setMaximumSize(new Dimension(620, 655));
 		setFont(new Font("Tahoma", Font.PLAIN, 14));
 		setBounds(new Rectangle(0, 0, 620, 660));
 		setBorder(new LineBorder(new Color(0, 0, 0)));
@@ -41,21 +41,21 @@ public class ArtLoeschenView extends JPanel implements View{
 		setLayout(null);
 		
 		//B_L�schen
-		B_Loeschen = new JButton("L\u00F6schen");
+		B_Loeschen = new JButton("Löschen");
 		B_Loeschen.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		B_Loeschen.setBounds(440, 600, 150, 30);
 		add(B_Loeschen);
 		
 		//L_Bezeichnung
-		JLabel L_Bezeichnung = new JLabel("Bezeichnung:");
+		JLabel L_Bezeichnung = new JLabel("Bitte die zu löschende Art auswählen");
 		L_Bezeichnung.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		L_Bezeichnung.setBounds(30, 300, 150, 30);
+		L_Bezeichnung.setBounds(12, 13, 237, 30);
 		add(L_Bezeichnung);
 		
 		//J_Combobox -> Zur Auswahl der Art, die man l�schen m�chte
 		C_Art = new JComboBox<String>();
 		C_Art.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		C_Art.setBounds(200, 300, 300, 30);
+		C_Art.setBounds(261, 13, 322, 30);
 		add(C_Art);
 
 	}
