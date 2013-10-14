b2ee72a7b5d85d81fde99907068b06d27d570e3a
Jenny <Jenny@141.31.78.21>
2013-10-14 10:49:35 +0200

    vervollständigt

--- src/fallstudie/model/impl/Eintrag.java
+++ src/fallstudie/model/impl/Eintrag.java
@@ -1,74 +1,85 @@
 package fallstudie.model.impl;
+
 import java.sql.SQLException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.GregorianCalendar;
 
 import fallstudie.model.mysql.connector.RemoteConnection;
 
-/** CHANGELOG
- * @author Phil, 09.10.2013
- * generiert + implements (Interface) wurde entfernt, da Konstruktor nicht möglich ist im Interface
+/**
+ * CHANGELOG
+ * 
+ * @author Phil, 09.10.2013 generiert + implements (Interface) wurde entfernt,
+ *         da Konstruktor nicht möglich ist im Interface
  * @author Phil
  * @date 11.10.2013
+ * @author Jenny
  * @version 1.1 - Aktualisiert
  */
 
 public class Eintrag {
 
-	
 	private Date datum;
 	private Arbeitsgruppe arbeitsgruppe;
 	private int anzahl;
 	private Art art;
 	private int kalenderwoche;
 	private int kalenderjahr;
-	
-	//-----------------------------------------------------------
-	//---------------------KONSTRUKTOR--------------------------
-	//-----------------------------------------------------------
+
+	// -----------------------------------------------------------
+	// ---------------------KONSTRUKTOR--------------------------
+	// -----------------------------------------------------------
 	/**
 	 * Eintrag wird INSERTERT in die Datenbank
+	 * 
 	 * @param kalenderjahr
 	 * @param kalenderwoche
 	 * @param anzahl
 	 * @param arbeitsgruppe
 	 * @param art
 	 */
-	public Eintrag(int kalenderjahr, int kalenderwoche, int anzahl, 
+	public Eintrag(int kalenderjahr, int kalenderwoche, int anzahl,
 			Arbeitsgruppe arbeitsgruppe, Art art) {
-		
+
 		// TODO Auto-generated method stub
-		
-		try //Kontrolle ob Datenbankverbindung da
+
+		try // Kontrolle ob Datenbankverbindung da
 		{
-			if( RemoteConnection.connection == null || RemoteConnection.sql == null ){
+			if (RemoteConnection.connection == null
+					|| RemoteConnection.sql == null) {
 				RemoteConnection.connect();
-			};
-			
-		}
-		catch (NullPointerException e)
-		{
+			}
+			;
+
+		} catch (NullPointerException e) {
 			System.err.println(e.getMessage());
 			System.err.println("Konnte keine Datenbankverbindung herstellen!");
 		}
-		
-		// TODO Datum generieren
-		datum = today;
-		
-		try
-		{
-			System.out.println("INSERT INTO Eintrag (Kalenderjahr, Kalenderwoche, Summe, Arbeitsgruppe, Art, Datum)"  +
-					"VALUES ('"+kalenderjahr+"','"+kalenderwoche+"','"+anzahl+"','"+arbeitsgruppe+"','"+art+"','"+datum+"'");
 
-				int RowsAffected = RemoteConnection.sql.executeUpdate(
-						"INSERT INTO Eintrag (Kalenderjahr, Kalenderwoche, Summe, Arbeitsgruppe, Art, Datum) VALUES ('"+kalenderjahr+"','"+kalenderwoche+"','"+anzahl+"','"+arbeitsgruppe+"','"+art+"','"+datum+"'");
-				System.out.println("Rows Affected: "+RowsAffected+"");
-			
+		// heutiges Datum vom System übergeben
+		SimpleDateFormat simpleFormat = new SimpleDateFormat("yyyy-MM-dd");
+		Date date = new Date();
+		String datum = simpleFormat.format(date);
+
+		try {
+			System.out
+					.println("INSERT INTO Eintrag (Kalenderjahr, Kalenderwoche, Summe, Arbeitsgruppe, Art, Datum)"
+							+ "VALUES ('" + kalenderjahr + "','" + kalenderwoche + "','" + anzahl
+							+ "','" + arbeitsgruppe + "','" + art + "','" + datum + "'");
+
+			int RowsAffected = RemoteConnection.sql
+					.executeUpdate("INSERT INTO Eintrag (Kalenderjahr, Kalenderwoche, Summe, Arbeitsgruppe, Art, Datum) VALUES ('"
+							+ kalenderjahr + "','" + kalenderwoche + "','" + anzahl + "','" + arbeitsgruppe
+							+ "','" + art + "','" + datum + "'");
+			System.out.println("Rows Affected: " + RowsAffected + "");
+
 		}
-			
-			
-		catch (SQLException e) 
-		{
-				System.err.println(e.getMessage());
-				System.err.println("SQL Statement ist fehlerhaft!");
+
+		catch (SQLException e) {
+			System.err.println(e.getMessage());
+			System.err.println("SQL Statement ist fehlerhaft!");
 		}
 	}
 
