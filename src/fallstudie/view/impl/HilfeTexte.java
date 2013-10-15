1069df01bcc74c48a068713394d5ec770ce0eff1
Sari S <Sari S@141.31.68.39>
2013-10-15 11:01:31 +0200

    Hilfetexte

---
+++ src/fallstudie/view/impl/HilfeTexte.java
@@ -0,0 +1,23 @@
+package fallstudie.view.impl;
+
+public abstract class HilfeTexte {
+	
+	public static String WelcomeView = "Um eine Aktion durchführen zu können wählen Sie durch einen Mausklick einen Punkt aus dem Baum aus.";
+	public static String LoginView="In das Feld 'Benutzername' geben Sie den Ihnen zugewiesenen Benutzername ein. In das Feld 'Passwort' geben Sie Ihr Passwort ein.";
+	public static String BereichLoeschenView="Wählen Sie mithilfe des Pfeiles einen Bereich aus der DropDownBox aus und drücken Sie den 'Löschen' Knopf"; 
+	public static String KonfigurationView="In das Feld 'Anzahl der Monate vor dem Löschen der Einträge' geben sie als Zahl die Anzahl der Monate an die Sie löschen wollen. Danach bestätigen Sie mit dem 'Speichern' Knopf";
+	public static String DruckenView="Um die Übersicht zu drucken drücken Sie auf den 'Drucken' Knopf.";
+	public static String ArtLoeschenView="Drücken Sie auf den Pfeil der DropDownBox und wählen die gewünschte Art aus. Dann bestätigen Sie mit 'Löschen' Knopf.";
+	public static String ArtAnlegenView="Tragen Sie in das Feld 'Bezeichnung' den Namen der Art an die Sie anlegen wollen. Bestätigen Sie mit 'Speichern'.";
+	public static String ErfassenView="Das aktuelle Kalenderjahr ist bereits im Feld vorbelegt. Darunter können Sie wählen ob Sie für die aktuelle oder vorherige Woche einen Eintrag erfassen wollen. Durch den Pfeil der DropDownBow wählen Sie die Art aus. In dem Feld 'Erfassen' können Sie entweder mit der Tastatur direkt in das Feld die erfasste Zahl eingeben oder nach jedem bearbeiteten Fall die Zahl der Erfassungen um eins durch den + Knopf erhöhen.";
+	public static String PasswortaendernView="In das Feld 'Altes Passwort' geben Sie das Passwort ein, mit dem Sie sich bisher eingeloggt haben. In das Feld 'Neues Passwort' geben Sie das von Ihnen frei gewählte neue Passwort ein. Dieses bestätigen Sie nochmals in dem Feld 'Neues Passwort wiederholen'. Dann bestätigen Sie mit dem 'Speichern' Knopf.";
+	public static String ArbeitsgruppeBearbeitenAnlegenView="In das Feld 'Kurzbezeichnung' geben Sie die Kurzbezeichnung der Arbeitsgruppe ein. In dem Feld 'Bezeichnung' die entsprechende Bezeichnung. Den Gruppenleiter können Sie durch einen Klick auf den 'Suchen' Knopf auswählen. Durch einen Klick auf den Pfeil der DropDownBox können Sie einen Bereich auswählen.";
+	public static String BereichBearbeitenAnlegenView="Geben Sie die Kurzbezeichnung und Bezeichnung des Bereichs in die jeweiligen Felder ein. Den gewünschten Bereichsleiter geben Sie in das Feld 'Bereichsleiter' ein und klicken auf den 'Suchen' Knopf. ";
+	public static String DatenAnzeigenArbeitsgruppeView="Um in der Tabelle eventuell vorhandene weitere Einträge zu sehen scrollen Sie mit der Scrollbar direkt neben der Tabelle nach unten.";
+	public static String DatenAnzeigenAuswahlView="Wenn Sie die Daten eines ganzen Kalenderjahres anzeigen lassen wollen, lassen Sie das Feld 'Kalenderwoche' leer. In das Feld 'Kalenderjahr' geben Sie mit der Tastatur das gewünschte Jahr ein und bestätigen mit einem Klick auf den 'Weiter' Knopf. Ansonsten geben Sie das gewünschte Kalenderjahr mit der zugehörigen Kalenderwoche in die jeweiligen Felder ein.";
+	public static String DatenAnzeigenBereichView="Wenn Sie von einem Bereich die Daten aller Arbeitsgruppen anzeigen lassen wollen drücken Sie auf den RadioButton am Ende der jeweiligen Zeile.";
+	public static String MitarbeiterAnlegenView="Geben Sie Vorname,Nachname und Benutzername des anzulegenden Mitarbeiters an. Rolle und Bereich können Sie dem Mitarbeiter zuordnen durch einen Klick auf den Pfeil der jeweiligen DropDownBox. Eine Arbeitsgruppe können Sie dem Mitarbeiter durch einen Klick auf den Knopf 'Suchen' neben dem Feld Arbeitsgruppe zuordnen. Für den neuen Mitarbeiter muss ein Passwort festgelegt werden. Entweder lassen sie Sich durch einen Klick auf den Knopf 'Generieren' ein Passwort automatisch erstellen oder Sie geben selbst in das Feld ein von Ihnen gewähltes Passwort ein.";
+	public static String SuchenView="Geben Sie den Suchbegriff in das Feld ein und bestätigen Sie mit einem Klick auf den 'Suchen' Knopf.";
+	public static String MitarbeiterBearbeitenView="Klicken Sie in das Feld in dem Sie eine Änderung vornehmen wollen und geben Sie die Änderung ein. Dann bestätigen Sie mit 'Speichern'.";
+	
+}
