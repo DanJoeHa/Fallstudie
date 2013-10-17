0c33bca0e126271b2f4165bc97c9494c69c33b42
MarcMoessinger <marc_24_12@web.de>
2013-10-17 08:47:14 +0200

    sdf

--- src/fallstudie/controller/impl/MitarbeiterController.java
+++ src/fallstudie/controller/impl/MitarbeiterController.java
@@ -294,10 +294,8 @@ public class MitarbeiterController implements Controller {
 			this.viewAnlegen = new MitarbeiterAnlegenView();
 			this.viewAnlegen.setController( this );
 			
-			System.out.println("lalala");
 			//ausgewählte Arbeitsgruppe holen
 			this.gewaehlteAG = (Arbeitsgruppe) this.suche.getAuswahl();
-			System.out.println("asdfasd");
 			this.viewAnlegen.setRolle( Funktionen.RollenCollection2Array(this.rollen) );
 			this.viewAnlegen.setBereich(Funktionen.BereicheCollection2Array(this.bereiche));
 			this.viewAnlegen.setArbeitsgruppe(gewaehlteAG.getKurzbezeichnung());
