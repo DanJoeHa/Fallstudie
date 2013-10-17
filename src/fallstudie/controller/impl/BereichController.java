69027ae49ce7a86875e0ffe54b08fa990b06c656
Johannes <johannes.d.haag@web.de>
2013-10-17 15:09:10 +0200

    Bereich ohne Leiter anlegen/bearbeiten

--- src/fallstudie/controller/impl/BereichController.java
+++ src/fallstudie/controller/impl/BereichController.java
@@ -3,9 +3,7 @@ package fallstudie.controller.impl;
 import java.awt.event.ActionEvent;
 import java.util.Collection;
 import java.util.Iterator;
-
 import fallstudie.controller.interfaces.Controller;
-import fallstudie.model.impl.Art;
 import fallstudie.model.impl.Bereich;
 import fallstudie.model.impl.Mitarbeiter;
 import fallstudie.view.impl.BereichBearbeitenAnlegenView;
@@ -20,60 +18,113 @@ public class BereichController implements Controller {
 	private BereichBearbeitenAnlegenView view;
 	private BereichLoeschenView viewLoesch;
 	private SuchController suche;
-	private Mitarbeiter gewaehlterMA;
+	private Mitarbeiter gewaehlterMA, oLeiter;
 	private Bereich gewaehlterBereich;
 	public static BestaetigenPopup popup;
-	private View aktView;
-	
+
 	private Collection<Bereich> bereich;
 	
+	/**
+	 * Bereiche in Controller laden
+	 * 
+	 */
 	public BereichController(){
 		this.bereich = Bereich.getAlleBereiche();
 	}
 	
+	/**
+	 * Operation (anlegen, loeschen, bearbeiten) bestimmen
+	 * 
+	 * 
+	 * @param operation ("anlegen" OR "loeschen" OR "bearbeiten")
+	 */
 	public void setOperation(String operation){
+		//Operation speichern
 		this.operation = operation;
-		if( this.operation.equals("anlegen"))
-		{
+		
+		//neuen Bereich anlegen
+		if( this.operation.equals("anlegen")){
 			this.view = new BereichBearbeitenAnlegenView();
 			this.view.setController( this );
 		}
-		else
-		{
-			if(this.operation.equals("loeschen"))
-			{
-				this.viewLoesch = new BereichLoeschenView();
-				this.viewLoesch.setController( this );
-				this.viewLoesch.setBereiche(Funktionen.BereicheCollection2Array( this.bereich ));
-			}
-			else
-			{
-				if(this.operation.equals("bearbeiten"))
-				{
-					this.viewLoesch = new BereichLoeschenView();
-					this.viewLoesch.setController( this );
-					this.viewLoesch.setBereiche(Funktionen.BereicheCollection2Array(Bereich.getAlleBereiche()));
-					this.viewLoesch.setButtonName("Bearbeiten");
-					this.viewLoesch.setHinweis("Bitte zu bearbeitenden Bereich auswählen");
-				}
-			}
+		
+		//Bereich löschen
+		if(this.operation.equals("loeschen")){
+			this.viewLoesch = new BereichLoeschenView();
+			this.viewLoesch.setController( this );
+			this.viewLoesch.setBereiche(Funktionen.BereicheCollection2Array( this.bereich ));
+		}
+		
+		//Bereich bearbeiten
+		if(this.operation.equals("bearbeiten")){
+			this.viewLoesch = new BereichLoeschenView();
+			this.viewLoesch.setController( this );
+			this.viewLoesch.setBereiche(Funktionen.BereicheCollection2Array(Bereich.getAlleBereiche()));
+			this.viewLoesch.setButtonName("Bearbeiten");
+			this.viewLoesch.setHinweis("Bitte zu bearbeitenden Bereich auswählen");
 		}
 	}
 	
+	/**
+	 * Bestimmen welcher Button gedrückt wurde und entsprechende Funktion ausführen
+	 * 
+	 */
 	public void actionPerformed(ActionEvent e) {
 		String button = e.getActionCommand();
 		
-		if(button.equals("Löschen") )
-		{
+		//Bereich löschen bestätigten
+		if(button.equals("Löschen") ){
 			popup = new BestaetigenPopup();
 			popup.setController(this);
 			popup.setTitle("Löschen");
 			popup.setAusgabe(HilfeTexte.LoeschenPopup);
-			
-			aktView = this.viewLoesch;
 		}
-		if(aktView == this.viewLoesch){
-		if(button.equals("Ja")){
+		
+		//Bereich anlegen/bearbeiten speichern
+		if(button.equals("Speichern")){	
+			popup = new BestaetigenPopup();
+			popup.setController(this);
+			
+			//Leiter holen
+			if( !this.view.getLeiter().isEmpty() ) oLeiter = this.gewaehlterMA;	
+			
+			//Prüfung ob Leiter ersetzt wird
+			boolean replace = false;
+			
+			//Bereichsleiter bei Bearbeiten ersetzen, wenn ein Leiter gesetzt war
+			if( this.operation == "bearbeiten" && this.gewaehlterBereich.getLeiter() != null ){
+					
+				//Bereich soll keinen Leiter mehr haben
+				if( oLeiter == null ){
+					replace = true;
+				}else{
+					//neuer Leiter != neuem Leiter
+					if( !this.gewaehlterBereich.getLeiter().getBenutzername().equals( oLeiter.getBenutzername() ) ) replace = true;
+				}
+
+			}
+			
+			//Ersetzen-Popup anzeigen
+			if(replace){
+				//Ersetzen-PopUp
+				popup.setTitle("Bereichsleiter ersetzen?");
+				popup.setAusgabe("Wollen Sie den aktuellen Bereichsleiter ersetzen und speichern?");
+				popup.setButtonName( "Ersetzen", "Nicht ersetzen" );
+				
+			//Wirklich speichern Popup anzeigen
+			}else{
+				popup.setTitle("Bestätigung");
+				popup.setAusgabe(HilfeTexte.SpeichernPopup);
+			}
+			
+		}
+		
+
+			
+		//Bereich löschen durchführen
+		if( this.operation.equals("loeschen") && button.equals("Ja") ){
+			
+			//Bereich zum löschen finden und löschen
 			Iterator<Bereich> i = this.bereich.iterator();		
 			while( i.hasNext() ){
 				Bereich B = (Bereich) i.next();
@@ -87,14 +138,19 @@ public class BereichController implements Controller {
 					break;
 				}
 			}
+			
+			//Popup wieder ausblenden
 			popup.setVisible(false);
 		}
-		if(button.equals("Nein")){
+			
+		//Abbrechen Lösch-Aktion
+		if(button.equals("Nein") || button.equals("Nicht ersetzen")){
 			popup.setVisible(false);
 		}
-		}
-		if(button.equals("Bearbeiten") )
-		{
+		
+		//Bereich zur Bearbeitung ausgewählt
+		if(button.equals("Bearbeiten") ){
+			
 			String tempBereich = this.viewLoesch.getBereich();
 			this.view = new BereichBearbeitenAnlegenView();
 			HauptController.hauptfenster.setContent(view);
@@ -114,8 +170,8 @@ public class BereichController implements Controller {
 		}
 		
 		//Bereichsleiter suchen
-		if(button.equals("Suchen"))
-		{
+		if(button.equals("Suchen")){
+			
 			this.suche = new SuchController();
 			this.suche.setAufrufenderController(this);
 			this.suche.setSuchdomain("Bereichsleiter");
@@ -126,49 +182,42 @@ public class BereichController implements Controller {
 			HauptController.hauptfenster.setContent(this.suche.getView());
 		}
 		
-		if(button.equals("Speichern"))
-		{	
-			popup = new BestaetigenPopup();
+		//Bereich anlegen/bearbeiten durchführen
+		if(button.equals("Ja") || button.equals("Ersetzen")){	
 			
-			popup.setController(this);
-			popup.setTitle("Bestätigung");
-			popup.setAusgabe(HilfeTexte.SpeichernPopup);
-			
-			aktView = this.view;}
-			if(aktView == this.view){
-		if(button.equals("Ja")){
-			
-		
+			//Bereich anlegen
 			if(this.operation.equals("anlegen"))
 			{
 				try
 				{
-					new Bereich(this.view.getKurzbezeichnung(), this.view.getBezeichnung(), gewaehlterMA);
+					new Bereich(this.view.getKurzbezeichnung(), this.view.getBezeichnung(), oLeiter);
 				}
 				catch (Exception ex)
 				{
 					HauptController.hauptfenster.setInfoBox(ex.getMessage());
+				}finally{
+					popup.setVisible(false);
 				}
 			}
-			if(this.operation.equals("bearbeiten"))
-			{
+			
+			//Bereich bearbeiten
+			if(this.operation.equals("bearbeiten"))	{
+				
 				try
 				{
 					this.gewaehlterBereich.setBeschreibung(this.view.getBezeichnung());
 					this.gewaehlterBereich.setKurzbezeichnung(this.view.getKurzbezeichnung());
-					this.gewaehlterBereich.setLeiter(this.gewaehlterMA);
+					this.gewaehlterBereich.setLeiter(oLeiter);
 				}
 				catch (Exception ex)
 				{
 					HauptController.hauptfenster.setInfoBox(ex.getMessage());
+				}finally{
+					popup.setVisible(false);
 				}
-			}
-			popup.setVisible(false);
+			}					
 		}
-		if(button.equals("Nein")){
-			popup.setVisible(false);
-		}
-			}
+		
 	}
 
 	@Override
