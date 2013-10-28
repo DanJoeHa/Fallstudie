c1c97962735aced594d681c7d01485299192c401
pati19992 <patrick.lachnit@gmx.de>
2013-10-28 12:25:20 +0100

    kommi

--- src/fallstudie/view/impl/HilfeTexte.java
+++ src/fallstudie/view/impl/HilfeTexte.java
@@ -1,32 +1,116 @@
 package fallstudie.view.impl;
-
+/**
+ * Hilfetexte für Views und Texte für Popups
+ * @author Patrick Lachnit
+ *
+ */
 public abstract class HilfeTexte {
-	
+/**
+ * Hilfetext für WelcomeView	
+ */
 public static String WelcomeView = "Über den Navigationsbaum können Sie die gewünschte Aktion auswählen.";
+/**
+ * Hilfetext für LoginView	
+ */
 public static String LoginView = "Geben Sie hier den Ihnen zugewiesenen Benutzernamen und Ihr Passwort ein. Bei Ihrem ersten Login werden Sie dazu aufgefordert Ihr Passwort zu ändern. Falls Sie Ihr Passwort vergessen haben, wenden Sie sich bitte an den UHD unter der Nummer 06328.";
+/**
+ * Hilfetext für BereichLoeschenView	
+ */
 public static String BereichLoeschenView = "Wählen Sie mit Hilfe der Drop-Down-Box einen Bereich aus, um diesen zu löschen.";
+/**
+ * Hilfetext für KonfigurationsView	
+ */
 public static String KonfigurationView = "Geben Sie hier an, nach wie vielen Monaten die Einträge gelöscht werden sollen und bestätigen Sie dies.";
+/**
+ * Hilfetext für DruckenView
+ */
 public static String DruckenView = " Mit der Funktion „Drucken“ können Sie die angezeigte Tabelle als PDF-Datei ausgeben.";
+/**
+ * Hilfetext für ArtLoeschenView	
+ */
 public static String ArtLoeschenView = "Wählen Sie aus der Drop-Down-Box eine Art aus, um diese zu löschen.";
+/**
+ * Hilfetext für ArtAnlegenView	
+ */
 public static String ArtAnlegenView = "Tragen Sie die Bezeichnung der Art ein, die Sie anlegen wollen und bestätigen Sie dies.";
+/**
+ * Hilfetext für ErfassenView
+ */
 public static String ErfassenView = "Das aktuelle Kalenderjahr ist bereits vorgegeben. Es kann allerdings ausgewählt werden, ob Sie einen Eintrag  für die aktuelle oder für die vorherige Woche erfassen wollen. Über die Drop-Down-Box kann die gewünschte Art des Eintrags ausgewählt werden. Im Erfassen-Feld kann entweder direkt in das Feld eine Zahl eingegeben werden oder über + die Zahl im Feld hochgezählt werden.";
+/**
+ * Hilfetext für PasswortaenderView	
+ */
 public static String PasswortaendernView = "Geben Sie hier Ihr aktuelles Passwort und Ihr neues Passwort ein, wiederholen Sie dieses und bestätigen Ihre Änderung dann.";
+/**
+ * Hilfetext für ArbeitsgruppenBearbeitenAnelgenView
+ */
 public static String ArbeitsgruppeBearbeitenAnlegenView = "Für das Anlegen einer Arbeitsgruppe muss hier eine Kurzbezeichnung und die entsprechende Bezeichnung eingegeben werden. Der Gruppenleiter kann über „Suchen“ ausgewählt werden. Über die Drop-Down-Box kann der Bereich ausgewählt werden, zu dem die Arbeitsgruppe gehören soll.";
+/**
+ * Hilfetext für BereichBearbeitenAnlegenView
+ */
 public static String BereichBearbeitenAnlegenView = "Geben Sie hier die Kurzbezeichnung und die entsprechende Bezeichnung des Bereichs in die jeweiligen Felder ein. Der gewünschten Bereichsleiter kann über die Suche ausgewählt werden.";
+/**
+ * Hilfetext für DatenAnzeigenView	
+ */
 public static String DatenAnzeigenArbeitsgruppeView = " Mit der Funktion „Drucken“ können Sie die angezeigte Tabelle als PDF-Datei ausgeben.";
+/**
+ * Hilfetext für DatenAnzeigenAuswahlView
+ */
 public static String DatenAnzeigenAuswahlView = "Um die Daten eines ganzen Kalenderjahres anzuzeigen, geben Sie das gewünschte Kalenderjahr ein und lassen das Feld der Kalenderwoche frei. Ansonsten geben Sie das Kalenderjahr und die gewünschte Kalenderwoche in den jeweiligen Felder an.";
+/**
+ * Hilfetext für DatenAnzeigenBereichView
+ */
 public static String DatenAnzeigenBereichView = " Mit der Funktion „Drucken“ können Sie die angezeigte Tabelle als PDF-Datei ausgeben. Wenn Sie zu einem bestimmten Bereich die Ergebnisse der zugehörigen Arbeitsgruppen einsehen wollen, kann mit Hilfe der Drop-Down-Box der entsprechende Bereich ausgewählt werden. Mit der Funktion „Drill Down“ wird eine neue Tabelle mit den Ergebnissen der Arbeitsgruppen ausgegeben.";
+/**
+ * Hilfetext für MitarbeiterAnlegenView
+ */
 public static String MitarbeiterAnlegenView = "Geben Sie Vorname, Nachname und Benutzername Mitarbeiters an, den Sie anlegen wollen. Rolle und Bereich können Sie dem Mitarbeiter zuordnen, indem Sie diese über die jeweilige Drop-Down-Box auswählen. Über die Suche kann dem Mitarbeiter eine Arbeitsgruppe zugeordnet werden. Das Passwort des neuen Mitarbeiters kann entweder über „Generieren“ festgelegt werden oder selbst eingegeben werden. Beim ersten Login des Mitarbeiters muss das Passwort dann von ihm geändert werden.";
+/**
+ * Hilfetext für SuchenView
+ */
 public static String SuchenView = "Geben Sie den gesuchten Begriff in das Feld ein. Mit der Funktion „Suchen“ werden Sie auf eine Tabelle weitergeleitet, in der die entsprechenden Ergebnisse aufgelistet werden.";
+/**
+ * Hilfetext für MitarbeiterBearbeitenView	
+ */
 public static String MitarbeiterBearbeitenView = "Geben Sie in das gewünschte Feld Ihre Änderung ein und bestätigen Sie diese. Falls ein Passwort geändert werden soll, ist das neue Passwort in das entsprechende Feld einzutragen. Wird das Feld freigelassen, bleibt das Passwort dasselbe.";
+/**
+ * Hilfetext für SpeichernPopup	
+ */
 public static String SpeichernPopup = "Wollen Sie wirklich speichern?";
+/**
+ * Hilfetext für LoeschenPopup
+ */
 public static String LoeschenPopup = "Wollen Sie wirklich löschen?";
+/**
+ * Hilfetext für Tabelle_Arbeitsgruppeeinsehen_Gruppenleiter
+ */
 public static String Tabelle_Arbeitsgruppeeinsehen_Gruppenleiter = " Mit der Funktion „Drucken“ können Sie die angezeigte Tabelle als PDF-Datei ausgeben.";
+/**
+ * Hilfetext für Tabelle_Arbeitsgruppeeinsehen	
+ */
 public static String Tabelle_Arbeitsgruppeeinsehen = " Mit der Funktion „Drucken“ können Sie die angezeigte Tabelle als PDF-Datei ausgeben.";
+/**
+ * Hilfetext für Tabelle_SummierteErgebnisseGesamtbereich	
+ */
 public static String Tabelle_SummierteErgebnisseGesamtbereich = " Mit der Funktion „Drucken“ können Sie die angezeigte Tabelle als PDF-Datei ausgeben. Wenn Sie zu einem bestimmten Bereich die Ergebnisse der zugehörigen Arbeitsgruppen einsehen wollen, kann mit Hilfe der Drop-Down-Box der entsprechende Bereich ausgewählt werden. Mit der Funktion „Drill Down“ wird eine neue Tabelle mit den Ergebnissen der Arbeitsgruppen ausgegeben.";
+/**
+ * Hilfetext für Tabelle_SummierteErgebnisseGesamtbereich_AG	
+ */
 public static String Tabelle_SummierteErgebnisseGesamtbereich_AG = "Mit der Funktion „Drucken“ können Sie die angezeigte Tabelle als PDF-Datei ausgeben.";
+/**
+ * Hilfetext für Tabelle_Mitarbeiteranlegen_Mitarbeiterbearbeiten_Arbeitsgruppebearbeiten_AG	
+ */
 public static String Tabelle_Mitarbeiteranlegen_Mitarbeiterbearbeiten_Arbeitsgruppebearbeiten_AG = "Wählen Sie aus der Tabelle die gewünschte Arbeitsgruppe aus, um diese weiter zu bearbeiten.";
+/**
+ * Hilfetext für Tabelle_Mitarbeiterbearbearbeiten_Mitarbeiterloeschen_Arbeitsgruppeanlegen_Arbeitsgruppebearbeiten_Gruppenleiter_Bereichanlegen_Bereichsleiter_Bereichbearbeiten_Bereichsleiter	
+ */
 public static String Tabelle_Mitarbeiterbearbearbeiten_Mitarbeiterloeschen_Arbeitsgruppeanlegen_Arbeitsgruppebearbeiten_Gruppenleiter_Bereichanlegen_Bereichsleiter_Bereichbearbeiten_Bereichsleiter = "Wählen Sie aus der Tabelle den gewünschten Mitarbeiter aus.";
+/**
+ * Hilfetext für Tabelle_Arbeitsgruppeloeschen	
+ */
 public static String Tabelle_Arbeitsgruppeloeschen = "Wählen Sie aus der Tabelle die gewünschte Arbeitsgruppe aus, um diese zu löschen.";
+/**
+ * Hilfetext für BereichBearbeiten	
+ */
 public static String BereichBearbeiten = "Mit Hilfe der Drop-Down-Box können Sie den gewünschten Bereich auswählen, um diesen zu bearbeiten.";
 }
